c2f0ae67fcfc3e45818a24d0777942fef3c8a1d7
henryjones924 <henryjones2724@gmail.com>
2024-12-06 18:52:47 -0500

    Day5 Part1

--- 2024/05/solution.py
+++ 2024/05/solution.py
@@ -1,10 +1,61 @@
 import helper as helper
 
 
+def extract_rules_and_updates(data):
+    rules = []
+    updates = []
+    clean_list = [line.strip() for line in data.splitlines()]
+    for line in clean_list:
+        rule = []
+        update = []
+        if len(line) == 5:
+            rule += line.split("|")
+            rules.append(rule)
+        else:
+            update += line.split(",")
+            if len(update) > 1:
+                updates.append(update)
+            print(update)
+    return rules, updates
+
+
+def rule_checker(update, rules):
+    for a, b in rules:
+        a_index = False
+        b_index = False
+        if a in update:
+            a_index = update.index(a)
+        if b in update:
+            b_index = update.index(b)
+
+        # print(a_index)
+        if a_index is False or b_index is False:
+            pass
+        elif a_index < b_index:
+            pass
+        else:
+            return False
+    print(update)
+    middle_index = (len(update)) // 2
+    middle = update[middle_index]
+    print(middle)
+    return int(middle)
+
+
 @helper.performance
 @helper.debug
 def solution_p1(data):
-    return "null"
+    total = 0
+    rules, updates = extract_rules_and_updates(data)
+
+    for update in updates:
+        valid = rule_checker(update, rules)
+
+        if valid is False:
+            pass
+        else:
+            total += valid
+    return total
 
 
 @helper.performance
